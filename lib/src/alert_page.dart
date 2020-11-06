ada8eb39f097a19a8c4110f6ed32791ec52d6a03
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-11-06 17:20:21 -0500

    add alertDialog

--- lib/src/alert_page.dart
+++ lib/src/alert_page.dart
@@ -8,7 +8,43 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alertas'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context))
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
+      ),
     );
   }
   
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) => 
+      AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        title: Text('Alerta'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Este es el contenido de la Alerta'),
+            FlutterLogo(size: 100.0)
+          ]
+        ),
+        actions: <Widget>[
+          FlatButton(onPressed: () => Navigator.of(context).pop(), 
+          child: Text('Cancelar')),
+          FlatButton(onPressed: () => Navigator.of(context).pop(),
+          child: Text('Aceptar'))
+        ],
+      ),
+      );
+  }
 }
